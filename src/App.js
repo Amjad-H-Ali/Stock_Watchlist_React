5648abbb9822889f0c77727860edc8db8860667a
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-05-28 19:46:05 -0500

    Added styling to homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="indices-header">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <header className="app-header">
+          <div className="watchlist-link nav">Watchlist</div>
+          <div className="app-title">Foo Finance</div>
+          <div className="sign-link nav">Sign In</div>
+        </header>
+
+        <div className="search-field">
+          <input className="search-bar" type="search" placeholder="Search Ticker"/>
+          <div className="search-icon"><i className="fa fa-search"></i></div>
+        </div>
+
+        <div className="app-body">
+          <div className="watchlist-btn-container"><button className="add-to-watchlist-btn"></button></div>
+
+          <div className="stock-info">
+            <div><span className="stock-title">Apple Inc.</span> <span className="stock-ticker">(APPL)</span></div>
+            <div><span className="stock-price">189.28</span> <span className="stock-percentage">1.14(0.61%)</span></div>
+            <div><div className="market-cap-title">Market Cap</div> <div className="stock-market-cap">930.043 B</div></div>
+          </div>
+
+          <div></div>
+        </div>  
+       
       </div>
     );
   }
