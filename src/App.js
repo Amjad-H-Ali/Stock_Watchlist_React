3e20a711fa5910d4202dc4680fef96f628d9d4dc
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-05 08:48:42 -0500

    Fixed bug in addToWatchlist function where new stock will not appear in watchlist

--- src/App.js
+++ src/App.js
@@ -165,14 +165,9 @@ class App extends Component {
       const percentified = this.percentify(changePercent);
 
       const stock = {symbol, companyName, latestPrice, iexBidPrice, iexAskPrice, change, changePercent:percentified, high, low, iexVolume, latestTime}
-
-      // if (watchedStocks.findIndex(aStock => aStock.symbol === stock.symbol) === -1) {
         
         this.setState({watchedStocks:[...watchedStocks, stock]});
-      // }
-      // else {
-      //   this.setState({watchlistShowing:true});
-      // },  
+       
     }
 
     this.setState({watchlistShowing:true});
@@ -198,7 +193,7 @@ class App extends Component {
 
   addToWatchlist = async () => {
 
-    const {logged} = this.state;
+    const {logged, watchedTickers} = this.state;
     if (logged) {
       let ticker;
 
@@ -220,7 +215,9 @@ class App extends Component {
 
       const {added_stock} = await addStockJSON.json();
 
-      this.getWatchedStocksInfo(added_stock);
+      this.setState({watchedTickers:[...watchedTickers, added_stock]});
+
+      this.getWatchedStocksInfo();
     }
     else {
       this.setState({modal:true, message:'You Must Be Logged In First!'})
@@ -276,52 +273,5 @@ class App extends Component {
 }
 
 export default App;
-/*
-
-        */
-
-
-
-
-/*
-
-
-
-
-        <div id="myModal" class="modal">
-
-          <div class="modal-content">
-            <div class="modal-header">
-              <span class="close">&times;</span>
-              <h2 className="modal-title">Sign In</h2>
-            </div>
-
-            <div class="modal-body">
-              <form className="sign-form">
-                <div className="tabs"> <div className="sign-tab">Sign In</div> <div className="register-tab"> Register</div> </div>
-                <div className="email-input-div"><div className="label">Email</div><input className="email-input" type="text" placeholder="Enter Your Email"/></div>
-                <div className="password-input-div"><div className="label">Password</div><input className="password-input" type="text" placeholder="Enter Your Password"/></div>
-                <div className="submit-btn-input-div"><button className="submit-btn">Sign In</button></div>
-              </form>  
-            </div>
-          </div>
-        </div> 
-*/        
-/*        <div className="search-field">
-          <input className="search-bar" type="search" placeholder="Search Ticker"/>
-          <div className="search-icon"><i className="fa fa-search"></i></div>
-        </div>
-
-        <div className="app-body">
-          <div className="watchlist-btn-container"><button className="add-to-watchlist-btn">Watch<i className="fa fa-angle-right"></i></button></div>
-
-          <div className="stock-info">
-            <div><span className="stock-title">Apple Inc.</span> <span className="stock-ticker">(APPL)</span></div>
-            <div><span className="stock-price">189.28</span> <span className="stock-percentage">1.14(0.61%)</span></div>
-            <div><div className="market-cap-title">Market Cap</div> <div className="stock-market-cap">930.043 B</div></div>
-          </div>
-
-          <div></div>
-        </div> 
-*/        
+        
 
